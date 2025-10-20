5672652a412e26afefe7eedb09539744a6ee73dc
Michael Popovich <earthmabus@hotmail.com>
2025-10-20 11:51:07 -0400

    added the ability to book for a spin class next tuesday at 6pm and verify the booking.

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import os
 import re
+import datetime as dt
+from dateutil.relativedelta import relativedelta
 
 from selenium.webdriver.support.wait import WebDriverWait
 
@@ -15,6 +17,13 @@ GYM_URL = "https://appbrewery.github.io/gym/"
 
 MAX_ELEMENT_WAIT_TIME_IN_SEC = 5
 
+DAY_OF_WEEK_MONDAY = 0
+DAY_OF_WEEK_TUESDAY = 1
+DAY_OF_WEEK_WEDNESDAY = 2
+DAY_OF_WEEK_THURSDAY = 3
+DAY_OF_WEEK_FRIDAY = 4
+DAY_OF_WEEK_SATURDAY = 5
+DAY_OF_WEEK_SUNDAY = 6
 
 def login():
     # wait until the login button becomes present
@@ -43,7 +52,7 @@ def handle_wait():
 # extracts class, date, and time from a button name
 # example name: "book-button-spin-2025-10-25-0800"
 # - class: spin
-# - date: 2025-10-25
+# - datetime: 2025-10-25
 # - time: 0800
 # returns None if the format is invalid
 def extract_class_date_time(button_name: str):
@@ -53,15 +62,93 @@ def extract_class_date_time(button_name: str):
     if match:
         cls = match.group("class")
         date = match.group("date")
-        time = match.group("time")
+        tm = match.group("time")
         return {
+            "button_name": button_name,
             "class": cls,
-            "date": date,
-            "time": time
+            "datetime": dt.datetime.strptime(f'{date}-{tm}', "%Y-%m-%d-%H%M")
         }
 
     return None
 
+def book_or_joinwait_for_next_tuesday_6pm_class():
+    elem_booking_elements = driver.find_elements(By.CSS_SELECTOR, "[id^='book']")
+    for e in elem_booking_elements:
+        c = extract_class_date_time(e.get_attribute('id'))
+
+        if c['datetime'].weekday() == DAY_OF_WEEK_TUESDAY and c['datetime'].hour == 18:
+            print(c)
+            e.click()
+            print(f'clicked on {c["button_name"]}')
+
+            # verify the course was booked
+            confirm = verify_booking(c)
+            if confirm:
+                print("you are verified for the class")
+
+            break
+
+def verify_booking(course_booking_dict):
+    all_bookings = get_all_bookings()
+
+    retval = False
+    for booking in all_bookings:
+        if booking['class'] == course_booking_dict['class'] and booking['datetime'] == course_booking_dict['datetime']:
+            retval = True
+            break
+
+    return retval
+
+# returns a list of { class, datetime, booking_status }
+def get_all_bookings():
+    '''Retrieves a list of classes that the user has booked according to the "My Bookings" page'''
+
+    # navigate to the "My Bookings" page
+    driver.implicitly_wait(1)
+    elem_mybookings_btn = driver.find_element(By.ID, "my-bookings-link")
+    elem_mybookings_btn.click()
+
+    # wait for the page to load
+    driver.implicitly_wait(1)
+
+    # create a list of dictionary of { class, datetime, booking_status}
+    # grab the full list of courses
+    all_bookings = []
+    elem_booking_elements = driver.find_elements(By.CSS_SELECTOR, "[id^='booking-card-booking_']")
+    for e in elem_booking_elements:
+        # navigate to each booking block and get the contents of the first <p> tag which should contain:
+        # "When: Mon, Oct 20, 5:00 PM"
+        first_p_elem = e.find_element(By.CSS_SELECTOR, "p")
+
+        text_when, text_datetime = first_p_elem.text.split(" ", 1)
+        #print(f"text_datetime = {text_datetime}")
+
+        # text_datetime = "Mon, Oct 20, 5:00 PM"
+        # add in the current year so that we can convert this to a datetime
+        # "When: Mon, Oct 20, 2025, 5:00 PM"
+        current_dt = dt.datetime.now()
+        text_datetime_with_year = re.sub(r"(\w{3}, \w{3} \d{1,2}),", rf"\1, {current_dt.year},", text_datetime)
+        #print(f"text_datetime_with_year = {text_datetime_with_year}")
+
+        # text_datetime_with_year = "Tue, Oct 21, 2025, 6:00 PM"
+        # Note: it's possible that the booking could actually be for NEXT year and not this year, thus the year that was just added in could be incorrect... if the month in text_datetime is less than the current month, that's probably the case and we should increment the year
+        booking_date = dt.datetime.strptime(text_datetime_with_year, "%a, %b %d, %Y, %I:%M %p")
+        if booking_date < current_dt:
+            booking_date = booking_date + relativedelta(years=1)
+
+        # get the booking_class
+        booking_class = e.get_attribute("data-class-type")
+
+        # get the status of the booking
+        booking_status = e.get_attribute("data-booking-status")
+
+        all_bookings.append({
+            "class": booking_class,
+            "datetime": booking_date,
+            "booking_status": booking_status
+        })
+
+    return all_bookings
 
 # configure chrome to stay open
 chrome_options = webdriver.ChromeOptions()
@@ -82,9 +169,4 @@ login()
 driver.implicitly_wait(2)
 
 # find the next Tuesday at 6 PM class and book it or join the wait list
-elem_booking_elements = driver.find_elements(By.CSS_SELECTOR, "[id^='book']")
-print(f'there are {len(elem_booking_elements)}')
-for e in elem_booking_elements:
-    print(f"{e.get_attribute('id')} - {e.text}")
-    print(extract_class_date_time(e.get_attribute('id')))
-
+book_or_joinwait_for_next_tuesday_6pm_class()
